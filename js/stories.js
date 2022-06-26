c053b98873322bcca3f445c6d821909da3a4ef20
Diana Huang <dianahuang@Dianas-MBP.lan>
2022-06-26 10:04:02 -1000

    css updates

--- js/stories.js
+++ js/stories.js
@@ -25,7 +25,7 @@ function generateStoryMarkup(story) {
   const hostName = story.getHostName();
   return $(`
       <li id="${story.storyId}">
-        <input type="checkbox">
+        <input type="checkbox" class="checkbox">
         <a href="${story.url}" target="a_blank" class="story-link">
           ${story.title}
         </a>
